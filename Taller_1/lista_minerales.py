db74b1f1f6324494eeb06c1840d8ab310f6a05ce
Valentina Rincón <v.rincono@uniandes.edu.co>
2023-08-26 14:51:34 -0500

    Punto 2.3 adelantamiento

--- Taller_1/lista_minerales.py
+++ Taller_1/lista_minerales.py
@@ -1,5 +1,33 @@
 import numpy as np 
+from mineral import Mineral
+lista= []
+f = open("Taller_1/minerales.txt","r", encoding="utf-8")
+file = f.readlines()
+for linea in file[1:17]:
+    atributos = linea.strip().split("\t")
+    nombre = atributos[0]
+    dureza = atributos[1]
+    rompimiento_por_fractura = atributos[2]
+    color = atributos[3]
+    composicion = atributos[4]
+    lustre = atributos[5]
+    specific_gravity = atributos[6]
+    sistema_cristalino = atributos[7]
+        
+    mineral_i = Mineral(nombre,dureza,rompimiento_por_fractura,color,composicion,lustre,specific_gravity,sistema_cristalino)
+        
+    lista.append(mineral_i)
+    
+def contar_silicatos():
 
-with open("Taller_1/minerales.txt","r", encoding="utf-8") as file:
-    for linea in file:
-        print(linea)
+    for mineral in lista:
+        print(mineral)
+        contador = 0
+
+        if mineral.silicato() is True:
+
+            contador +=1
+
+    return contador
+print(contar_silicatos())
+
