b7a0cc111af48076476f6cc222290bfd9cc4f916
JessiDaniL <jd.leonr1@uniandes.edu.co>
2023-08-26 12:39:19 -0500

    Punto 1.4

--- Taller_1/indices_refraccion.py
+++ Taller_1/indices_refraccion.py
@@ -46,7 +46,7 @@ def recibir_informacion_yml(archivo_yml)-> list:
 
 
 
-print(recibir_informacion_yml("Taller_1\Descargas\Adhesivos Ópticos\Loctite.yml"))
+#print(recibir_informacion_yml("Taller_1\Descargas 1-2\Adhesivos Ópticos\Loctite.yml"))
 
 #Falta revisar bien la ruta del archivo
 
@@ -54,27 +54,14 @@ print(recibir_informacion_yml("Taller_1\Descargas\Adhesivos Ópticos\Loctite.yml
 
 def auxiliar_categoria(ubicacion_archivo):
     
-    #Leer los archivos
-    with open('indices_refraccion.csv', 'r', newline='') as archivo_csv:
-    # Crea un lector CSV
-        lector = csv.reader(archivo_csv, delimiter='\t')  # Ajusta el delimitador según tu archivo CSV
-
-    # Inicializa una variable booleana para rastrear si es el primer ciclo
-        primer_ciclo = True
-
-    # Itera a través de las filas y descarga los archivos .yml
-        for fila in lector:
-            if primer_ciclo:
-                primer_ciclo = False
-                continue  # Omitir el primer ciclo
-
-            categoria = fila[0].split(',')[0]
+    cambio = ubicacion_archivo.split("/")
+    categoria = cambio[3].split(".")[-2]
         
     return categoria
 
-def graficar_indice_vs_longitud():
+def graficar_indice_vs_longitud(ruta:str):
     
-    archivo = recibir_informacion_yml()
+    archivo = recibir_informacion_yml(ruta)
     
     #Obtener la información de acuerdo con las posiciones del índice o la longitud según corresponda
     
@@ -85,41 +72,47 @@ def graficar_indice_vs_longitud():
         
     #Cálculo promedio
         
-    promedio = sum(eje_y)/len(eje_y)
-    desviación = np.std(eje_y)
+    promedio = round(sum(eje_y)/len(eje_y),3)
+    desviación = round(np.std(eje_y),3)
         
     #Gráfico de dispersión
         
-    plt.plot(eje_x,eje_y)
+    plt.scatter(eje_x,eje_y)
     
     #Obtención del título de la categoría
-    categoria = auxiliar_categoria() #Falta planear esta función auxiliar
+    categoria = auxiliar_categoria(ruta) #Falta planear esta función auxiliar
     #Nombre ejes
         
-    plt.title("Gráfica de {0}\n" "el n promedio es:{1} \n y su desviación estándar es:{2}").format(categoria,promedio,desviación)
+    plt.title("Gráfica de "+str(categoria) +" el n promedio es:" +str(promedio)+"\n" " y su desviación estándar es: " + str(desviación))
     plt.xlabel("Longitud de onda")
     plt.ylabel("Índice de refracción") 
+
     
 #Revisar ciclo que mande a cada imagen de acuerdo a su carpeta
 
     if categoria == "NOA1348.yml":
-        carpeta_guardar = "Taller_1/Adhesivos_Ópticos"
+        carpeta_guardar = "Taller_1/Descargas 1-2/Adhesivos Ópticos"
         os.makedirs(carpeta_guardar, exist_ok=True)
         ruta_guardar = os.path.join(carpeta_guardar, "NOA1348.png")
         plt.savefig(ruta_guardar)
         
     else:
-        carpeta_guardar = "Taller_1/Plásticos_Comerciales"
+        carpeta_guardar = "Taller_1/Descargas 1-2/Plásticos Comerciales"
         os.makedirs(carpeta_guardar, exist_ok=True)
         ruta_guardar = os.path.join(carpeta_guardar, "Kapton.png")
         plt.savefig(ruta_guardar)
-
-    plt.show()
         
-#archivo_1 = Taller_1/Plásticos_Comerciales/Kapton.png
-#archivo_2 = Taller_1/Adhesivos_Ópticos/ NOA1348.png
+    plt.show()
 
-graficar_indice_vs_longitud()
+
+
+lista_archivos = ["Taller_1/Descargas 1-2/Plásticos Comerciales/Kapton.yml", "Taller_1/Descargas 1-2/Adhesivos Ópticos/NOA1348.yml"]
+for elemento in lista_archivos:
+    
+    grafica = graficar_indice_vs_longitud(elemento)
+    
+    print(grafica) 
+    
 
 
 #Punto 1.5
