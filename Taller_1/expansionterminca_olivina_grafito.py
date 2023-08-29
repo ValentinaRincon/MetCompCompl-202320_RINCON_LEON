cbef43718fc22f6891fecb136d7c31ecba7594d0
Valentina Rincón <v.rincono@uniandes.edu.co>
2023-08-28 20:36:54 -0500

    Punto 2-5

---
+++ Taller_1/expansionterminca_olivina_grafito.py
@@ -0,0 +1,11 @@
+from expansiontermicamineral import *
+
+if __name__ == "__main__":
+    archivo_csv = 'Taller_1/graphite_mceligot_2016.csv'
+    
+    graphite = ExpansionTermicaMineral(archivo_csv)
+    
+if __name__ == "__main__":
+    archivo_csv = 'Taller_1/olivine_angel_2017.csv'
+    
+    olivine = ExpansionTermicaMineral(archivo_csv)
