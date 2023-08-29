75835533654c04f722199fcf24eee29c339ffc6d
Valentina Rincón <v.rincono@uniandes.edu.co>
2023-08-28 20:38:28 -0500

    Eliminación del archivo .sh

--- Taller_1/punto1_2.sh
+++
@@ -1,9 +0,0 @@
-input="indices_refraccion.csv"
-while IFS=, read -r categoria _ nombre link; do
-    if [ "$categoria" == "Categoria" ]; then
-        continue
-    fi
-    mkdir -p "Descargas/$categoria"
-    encoded_link=$(echo "$link" | sed 's/ /%20/g')
-    curl -L "$encoded_link" -o "Descargas/$categoria/$nombre.yml"
-done < "$input"
