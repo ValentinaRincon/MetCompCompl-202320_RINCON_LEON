61c37649098f3d925051ebb2459071bf279d3714
Valentina Rincón <123614238+ValentinaRincon@users.noreply.github.com>
2023-09-01 17:16:45 -0500

    Actividad valen

--- Clase_4/funciones_valen.py
+++ Clase_4/funciones_valen.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def posicion(A,k,m,t):
     '''
-    Esta función retorna la posición, x, en un tiempo de de una masa m atada a un
+    Esta función retorna la posición, x, en un tiempo de una masa m atada a un
     resorte de constante de fuerza k que inicialmente es desplazada A unidades de
     su posición de equilibrio. Esta función devuelve el resultado de 
 
@@ -21,3 +21,36 @@ def posicion(A,k,m,t):
     '''
     omega = np.sqrt(k/m)
     return A*np.sin(omega*t)
+
+def periodo(m,k):
+    
+    num= m/k
+    
+    return 2*np.pi*np.sqrt(num)
+
+def DerivadaDerecha(f,x,h):
+    
+    d = 0.
+    
+    if h != 0:
+        d = (f(x+h) - f(x))/h
+        
+    return d
+
+def DerivadaIzquierda(f,x,h):
+    
+    d = 0.
+    
+    if h != 0:
+        d = (f(x) - f(x-h))/h
+        
+    return d
+
+def DerivadaCentral(f,x,h):
+    
+    d = 0.
+    
+    if h != 0:
+        d = (f(x+h) - f(x-h))/(2*h)
+        
+    return d
