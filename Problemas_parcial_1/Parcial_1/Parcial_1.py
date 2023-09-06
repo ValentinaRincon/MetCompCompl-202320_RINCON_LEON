4137751d9ee66c254b2851ea011e40238aa07d3c
Valentina Rincón <123614238+ValentinaRincon@users.noreply.github.com>
2023-09-06 17:59:34 -0500

    continuancion del parcial

--- Problemas_parcial_1/Parcial_1/Parcial_1.py
+++ Problemas_parcial_1/Parcial_1/Parcial_1.py
@@ -19,7 +19,7 @@ def coeficientes(x_0,x_1,x_2,f):
     
     a = (((f(x_2)-f(x_1))/(x_2-x_1)) - (((f(x_1)-f(x_0))/(x_1-x_0)))/(x_2-x_0))
     
-    b = ((f(x_1)-f(x_0))/(x_1-x_0)) - ((x_0+x_1) * (f(x_2)-f(x_1))/(x_2-x_1) * a)
+    b = ((f(x_1)-f(x_0))/(x_1-x_0)) - ((x_0+x_1)  * a)
     
     c = f(x_0) - (x_0)*((f(x_1)-f(x_0))/(x_1-x_0)) + (x_0*x_1)*a
 
@@ -37,6 +37,54 @@ def x_3(d):
     deno =b + np.sqrt( b**2 - (4*a*c))
     
     x_3 = num/deno
-    
-    
+   
+    return x_3
 
+def met_Newton (f,df,xn,itmax=100,precision=1e-11):
+    
+    error = 1. #Valor de tipo float
+    it = 0 #La iteración inicia en 0
+    
+    while error > precision and it < itmax:
+        
+        try:
+            
+            xn1 = xn - f(xn)/df(f,xn)
+            #Criterio de parada
+            error = np.abs(f(xn)/df(f,xn))
+            
+        except ZeroDivisionError:
+            print('División por cero')
+            
+        xn = xn1
+        it += 1
+        
+    #print('Raiz',xn,it)
+    
+    if it == itmax:
+        return False
+    else:
+        return xn
+
+def todas_las_raices (x, tolerancia=10):
+    
+    raices = np.array([])
+    
+    for i in x:
+        
+        raiz = met_Newton(funcion,derivada,i)
+        
+        if raiz is not False:
+            
+            raiz_2 = np.round(raiz,tolerancia)
+            
+            if raiz_2 not in raices:
+                raices = np.append(raices,raiz_2)
+                
+    raices.sort()
+    
+    return raices
+
+raices = todas_las_raices(x)
+            
+print(raices[0],np.sqrt(3/5))
