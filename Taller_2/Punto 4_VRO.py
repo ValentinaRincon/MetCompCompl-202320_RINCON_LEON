94e1072af7bc717a12e0b167b5318155076a3cd8
Valentina Rincón <v.rincono@uniandes.edu.co>
2023-09-23 18:01:58 -0500

    Finalización del punto 4.1

--- Taller_2/Punto 4_VRO.py
+++ Taller_2/Punto 4_VRO.py
@@ -10,8 +10,8 @@ def montecarlo (a=0,b=np.pi):
     N = 10
     
     arregloN = np.linspace(10,10e3,9991)
-    
-    arregloE = []
+    arregloRaiz = np.array([])
+    arregloE = np.array([])
     
     while N <= 10000:
         
@@ -23,24 +23,35 @@ def montecarlo (a=0,b=np.pi):
         
         Iteo = 0.5*(1+np.exp(-np.pi))
         
-        error = (abs(Iteo - I))/I
+        error = ((abs(Iteo - I))/I)*100
         
-        arregloE.append(error)
+        arregloE = np.append(arregloE,error)
         
-        #print(arregloE)
-
+        calculo = 1/(np.sqrt(N))
+        
+        arregloRaiz= np.append(arregloRaiz,calculo)
+        
         N +=1
-    print("La longitud de arreglo del error es {0}".format(len(arregloE)))    
-    print("La longitud de arreglo de las N es {0}".format(len(arregloN)))    
-    fig, ax = plt.subplots()
-    ax.scatter(x = arregloN, y = arregloE)
-    #plt.scatter(arregloN,arregloE)
     
+#Parte gráfica
+
+    plt.figure(figsize=(10,4))
+    #_________________________________#
+    plt.subplot(1,2,1)
+    plt.scatter(x = arregloN, y = arregloE, c="#E2725B")
+    plt.title("Porcentaje de error vs N") 
+    plt.xlabel("N")
+    plt.ylabel("Porcentaje de error")
+    
+    #_________________________________#
+    plt.subplot(1,2,2)
+    plt.scatter(x = arregloN, y = arregloRaiz, c="#E2728B")
+    plt.title("N vs $\sqrt{N}$") 
+    plt.xlabel("$\sqrt{N}$")
+    plt.ylabel("N")
+    
+    #_________________________________#
+    plt.tight_layout()
     plt.show()
         
 montecarlo()
-
-"""
-Falta hacer la gráfica de 1/sqtr(N)
-Falta resolver el error para que dé 10e5 la gráfica. Buscar alternativa para el arreglo del error.
-"""
