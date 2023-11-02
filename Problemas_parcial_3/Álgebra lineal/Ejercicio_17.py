a63308cf08fd4a281e45c7071a5ce1d7dddf6526
Valentina Rincón <123614238+ValentinaRincon@users.noreply.github.com>
2023-11-01 19:20:02 -0500

    Avance punto 17 álgebra lineal

--- Problemas_parcial_3/Álgebra lineal/Ejercicio_17.py
+++ Problemas_parcial_3/Álgebra lineal/Ejercicio_17.py
@@ -17,26 +17,21 @@ f0,f1 = f(x,y)
 F = [f0,f1]
 F
 
-def GetJacobian(f,r,h=1e-6):
-    
-    n = r.shape[0]
-    
-    J = np.zeros((n,n))
-    
-    for i in range(n):
-        for j in range(n):
-            
-            rf = r.copy()
-            rb = r.copy()
-            
-            rf[j] = rf[j] + h
-            rb[j] = rb[j] - h
-            
-            J[i,j] = ( f[i](rf[0],rf[1],rf[2]) - f[i](rb[0],rb[1],rb[2])  )/(2*h)
-            
-    
-    return J
-u = GetJacobian(F,np.array([1.,1.,1.]))
-print(u)
+J = sym.zeros(2,2)
+
+for i in range(2):
+    for j in range(2):
+        if j==0:
+            J[i,j] = sym.diff(F[i],x,1)
+        else:
+            J[i,j] = sym.diff(F[i],y,1)
+
+J
 
 Fn = sym.lambdify([x,y],F,'numpy')
+
+J_i = np.linalg.inv(J)
+
+G = np.array([lambda x,y,z: 6*x - 2*np.cos(y*z) - 1.,
+     lambda x,y,z: 9*y + np.sqrt( x**2 + np.sin(z) + 1.06 ) + 0.9,
+     lambda x,y,z: 60*z + 3*np.exp(-x*y)+10*np.pi - 3])
