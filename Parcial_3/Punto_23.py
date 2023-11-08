74fe78c797c9afd7c8befa735d252377af58a32e
Valentina Rincón <123614238+ValentinaRincon@users.noreply.github.com>
2023-11-08 18:30:56 -0500

    Se completa el punto 23

--- Parcial_3/Punto_23.py
+++ Parcial_3/Punto_23.py
@@ -17,19 +17,17 @@ def conjugado (M,b,x_0,precision=0.01):
     p_0 = -r_0
     norma = np.linalg.norm(r_0)
     p = p_0
-    while norma >= precision and k < 6:
+    while norma >= precision and k < 5:
         m_p = np.dot(M,p)
         a = - (np.dot(r_0.T,p))/(np.dot(p.T,m_p))
         x_1 = x + np.dot(a,p)
         r_1 = np.dot(M,x_1)-b
-        print(r_1)
         b_1 = (np.dot(r_1.T,m_p))/(np.dot(p.T,m_p))
-        print(b_1)
         p_1 = (-r_1 + np.dot(b_1,p))
         p = p_1
         x = x_1
         k +=1
-        
+    print('Las iteraciones son {0}'.format(k))   
     print(x_1)   
     return x_1
 p = np.array([0,0,0,0,0])
